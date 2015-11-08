482ffee7d8e7c18a93a615820bc40896f0a2e713
theFool <saber.rl32@gmail.com>
2015-11-08 20:37:09 +0800

    Add Q47

---
+++ Permutations2/Permutations2.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    var len    = nums.length,
+        result = [],
+        used   = new Array(len),
+        temp   = new Array(len);
+
+    var _permuteUnique = function(i) {
+        if (i >= len) {
+            var r = [].concat(temp);
+            result.push(r);
+            return ;
+        }
+        var map = {};
+        for (var j = 0;j < len;++j) {
+            if (used[j]) {
+                continue;
+            }
+            if (!map[nums[j]]) {
+                used[j] = true;
+                temp[i] = nums[j];
+                _permuteUnique(i + 1);
+                used[j] = 0;
+                map[nums[j]] = 1;
+            }
+        }
+    };
+
+    _permuteUnique(0);
+    return result;
+};
