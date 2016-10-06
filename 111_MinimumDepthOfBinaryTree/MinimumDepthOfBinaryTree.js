a45cec4065c6932510b44efdf016e5aecc775723
theFool <saber.rl32@gmail.com>
2016-10-06 23:16:10 +0800

    Add Q110 Q111

---
+++ 111_MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+  if (!root) return 0
+  var queue = []
+  var result = 0
+  queue.push(root)
+  while (queue.length) {
+    ++result
+    for (var i = 0, l = queue.length; i < l;++i) {
+      var top = queue.shift()
+      if (!top.left && !top.right) return result
+      if (top.left) queue.push(top.left)
+      if (top.right) queue.push(top.right)
+    }
+  }
+  return result
+};
