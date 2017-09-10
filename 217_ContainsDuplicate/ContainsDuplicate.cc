76a15d4a1d21db3d5f27d8600228f80a44e675a5
theFool32 <saber.rl32@gmail.com>
2017-09-10 22:07:10 +0800

    Add Q216, Q217

---
+++ 217_ContainsDuplicate/ContainsDuplicate.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (auto &n : nums) {
+            if (s.find(n) == s.end()) {
+                s.insert(n);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
